550c796de1ccfa8b61487c90a5304d8f2e3736af
Dmitriy Belokurov <belokurovdmitriy@gmail.com>
2017-05-03 22:24:22 +0300

    Simple control buttons

--- config/globals.py
+++ config/globals.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 UNIVERSE_SIZE = 10000
-CANVAS_SIZE = 800
+CANVAS_SIZE = 650
 
--- main.py
+++ main.py
@@ -9,8 +9,16 @@ import config.globals as glob
 def main():
     root = tk.Tk()
     root.title("Gravitsapa")
+    zoom_in_button = tk.Button(root, text="Zoom In")
+    zoom_out_button = tk.Button(root, text="Zoom Out")
+    resume_button = tk.Button(root, text="Resume")
+    pause_button = tk.Button(root, text="Pause")
+    zoom_in_button.grid(row=0, column=0)
+    zoom_out_button.grid(row=0, column=1)
+    resume_button.grid(row=0, column=2)
+    pause_button.grid(row=0, column=3)
     canvas = tk.Canvas(root, width = glob.CANVAS_SIZE, height = glob.CANVAS_SIZE, bg='black')
-    canvas.pack()
+    canvas.grid(row=1, column=0, columnspan=4, sticky=tk.E+tk.W)
     root.mainloop()
 
 if __name__ == "__main__":
