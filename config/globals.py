1eec698090679a45cd9a3c44350cc8681db59a5c
Dmitriy Belokurov <belokurovdmitriy@gmail.com>
2017-05-03 17:22:11 +0300

    Fix encoding

--- config/globals.py
+++ config/globals.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 UNIVERSE_SIZE = 10000
 CANVAS_SIZE = 800
 
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import tkinter as tk
 from datetime import time
 from time import sleep
