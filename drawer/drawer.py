10c65c39095345dfd52f615e802a482682a95a3e
Eclyocy <ksanph@gmail.com>
2017-05-04 15:33:45 +0300

    Replaces tabs with spaces

--- drawer/drawer.py
+++ drawer/drawer.py
@@ -5,19 +5,19 @@ import config.globals as glob
 
 
 class Drawer:
-	def __init__(self, canvas):
-		self.canvas = canvas
+    def __init__(self, canvas):
+        self.canvas = canvas
 
-	def showObjects(self, celestial_objects):
-		self.canvas.delete("all")
-		canvas_x_center = self.canvas.winfo_width() / 2
-		canvas_y_center = self.canvas.winfo_height() / 2
-		zoom = glob.ZOOM
-		for obj in celestial_objects:
-			radius = math.log(obj.mass)
-			self.canvas.create_oval(canvas_x_center + zoom * (obj.x - radius),
-									canvas_y_center + zoom * (obj.y - radius),
-									canvas_x_center + zoom * (obj.x + radius),
-									canvas_y_center + zoom * (obj.y + radius), width=0,
-									fill=obj.color)
-		self.canvas.update()
+    def showObjects(self, celestial_objects):
+        self.canvas.delete("all")
+        canvas_x_center = self.canvas.winfo_width() / 2
+        canvas_y_center = self.canvas.winfo_height() / 2
+        zoom = glob.ZOOM
+        for obj in celestial_objects:
+            radius = math.log(obj.mass)
+            self.canvas.create_oval(canvas_x_center + zoom * (obj.x - radius),
+                                    canvas_y_center + zoom * (obj.y - radius),
+                                    canvas_x_center + zoom * (obj.x + radius),
+                                    canvas_y_center + zoom * (obj.y + radius),
+                                    width=0, fill=obj.color)
+        self.canvas.update()
